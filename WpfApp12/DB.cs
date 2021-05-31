6d7d2cc0a3cf85825411259ee74660c8bdfbed64
Ilya <kaproman00@mail.ru>
2021-05-31 03:05:22 +0300

    test

---
+++ WpfApp12/DB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp12
+{
+   public class DB
+    {
+        public static MedixEntities medixEntities = new MedixEntities();
+    }
+}
